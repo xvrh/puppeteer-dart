0a2f270c16550c5c6b4b11e77755bc2da9f3c44a
Xavier Hainaux <xavier.hainaux@gaming1.com>
2018-04-26 19:57:04 +0200

    Fix linux test

--- test/launch_test.dart
+++ test/launch_test.dart
@@ -1,9 +1,16 @@
 import 'package:chrome_dev_tools/chrome_dev_tools.dart';
 import 'package:chrome_dev_tools/chromium_downloader.dart';
 import 'package:chrome_dev_tools/domains/page.dart';
+import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
 main() {
+  Logger.root
+    ..level = Level.ALL
+    ..onRecord.listen(print);
+
+  //TODO(xha): test that if we start the same test in another process (dart launch_chrome),
+  // the process correctly exits.
   test('Can download and launch chromium', () async {
     String chromeExecutable = (await downloadChromium()).executablePath;
 
