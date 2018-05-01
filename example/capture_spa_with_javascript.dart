699382d555f4dcadd7ccac68d11d71f7166b67fe
Xavier Hainaux <xavier.hainaux@gaming1.com>
2018-05-01 21:13:59 +0200

    Don't use news.google.com

--- example/capture_spa_with_javascript.dart
+++ example/capture_spa_with_javascript.dart
@@ -2,7 +2,7 @@ import 'package:chrome_dev_tools/chrome_dev_tools.dart';
 import 'utils.dart';
 
 main() {
-  chromeTab('https://news.google.com', (Tab tab) async {
+  chromeTab('https://www.google.com', (Tab tab) async {
     // A small helper to wait until the network is quiet
     await tab.waitUntilNetworkIdle();
 
