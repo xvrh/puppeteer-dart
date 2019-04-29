ba881e54fa379d231fed61bc054df3b36bcd8f1f
Xavier Hainaux <xavier.hainaux@lampiris.be>
2019-04-29 12:40:58 +0200

    Add screencast example

--- tool/prepare_submit.dart
+++ tool/prepare_submit.dart
@@ -1,8 +1,10 @@
 import 'code_style/fix_absolute_import.dart' as fix_absolute_import;
 import 'code_style/fix_import_order.dart' as fix_import_order;
 import 'generate_readme.dart' as generate_readme;
+import 'inject_examples_to_doc.dart' as inject_examples_to_doc;
 
 main() {
+  inject_examples_to_doc.main();
   generate_readme.main();
   fix_absolute_import.main();
   fix_import_order.main();
