23b1a12a57cbef6f17732ce9e1a52f55190191cf
Xavier Hainaux <xavier.hainaux@yelido.be>
2019-05-03 09:57:46 +0200

    Make coverage.sh executable

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -1,15 +1,17 @@
+set -ex
+
 OBS_PORT=9292
 echo "Collecting coverage on port $OBS_PORT..."
 
 # Start tests in one VM.
 dart --enable-asserts \
-  --enable-vm-service=$OBS_PORT \
+  --enable-vm-service=${OBS_PORT} \
   --pause-isolates-on-exit \
   test/test_all.dart &
 
 # Run the coverage collector to generate the JSON coverage report.
 pub global run coverage:collect_coverage \
-  --port=$OBS_PORT \
+  --port=${OBS_PORT} \
   --out=var/coverage.json \
   --wait-paused \
   --resume-isolates
