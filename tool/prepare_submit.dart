14cfb836fd13e296416b3a18eb9e7124babf5aa0
xvrh <xavier.hainaux@gmail.com>
2019-05-03 15:24:56 +0200

    Add code coverage (#42)

--- tool/prepare_submit.dart
+++ tool/prepare_submit.dart
@@ -1,9 +1,11 @@
 import 'code_style/fix_absolute_import.dart' as fix_absolute_import;
 import 'code_style/fix_import_order.dart' as fix_import_order;
 import 'generate_readme.dart' as generate_readme;
+import 'generate_test_all_file.dart' as generate_test_all_file;
 import 'inject_examples_to_doc.dart' as inject_examples_to_doc;
 
 main() {
+  generate_test_all_file.main();
   inject_examples_to_doc.main();
   generate_readme.main();
   fix_absolute_import.main();
