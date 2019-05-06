4d9476b3770a81cf59ab152050adefb65e1e3ce3
Xavier Hainaux <xavier.hainaux@lampiris.be>
2019-05-06 17:02:13 +0200

    examples

---
+++ example/block_images.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'package:puppeteer/puppeteer.dart';
+
+main() async {
+  var browser = await puppeteer.launch();
+  var page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.onRequest.listen((request) {
+    if (request.resourceType == ResourceType.image) {
+      request.abort();
+    } else {
+      request.continueRequest();
+    }
+  });
+  await page.goto('https://news.google.com/news/');
+  var screenshot = await page.screenshot(fullPage: true);
+  await File('_news.png').writeAsBytes(screenshot);
+
+  await browser.close();
+}
---
+++ example/custom_event.dart
@@ -0,0 +1,28 @@
+import 'package:puppeteer/puppeteer.dart';
+
+main() async {
+  var browser = await puppeteer.launch();
+  var page = await browser.newPage();
+
+  // Define a window.onCustomEvent function on the page.
+  await page.exposeFunction('onCustomEvent', (Map e) {
+    print('${e['type']} fired');
+  });
+
+  // Attach an event listener to page to capture a custom event on page load/navigation.
+  listenFor(type) {
+    return page.evaluateOnNewDocument('''type => {
+      document.addEventListener(type, e => {
+        window.onCustomEvent({type, detail: e.detail});
+      });
+    }''', args: [type]);
+  }
+
+  // Listen for "app-ready" custom event on page load.
+  await listenFor('app-ready');
+
+  await page.goto('https://www.chromestatus.com/features',
+      wait: Until.networkIdle);
+
+  await browser.close();
+}
