43fc346d8c912701e736e1f8d2decf952cf5b59b
jakub-stefaniak <jakub.stefaniak@unitedideas.pl>
2021-10-19 13:08:31 +0200

    feat(downloader): run xattr -d com.apple.quarantine only on macos

--- lib/src/downloader.dart
+++ lib/src/downloader.dart
@@ -44,8 +44,12 @@ Future<RevisionInfo> downloadChrome({int? revision, String? cachePath}) async {
 
   if (!Platform.isWindows) {
     await Process.run('chmod', ['+x', executableFile.absolute.path]);
-    await Process.run(
-        'xattr', ['-d', 'com.apple.quarantine', executableFile.absolute.path]);
+  }
+
+  if (Platform.isMacOS) {
+    final chromeAppPath = executableFile.absolute.parent.parent.parent.path;
+
+    await Process.run('xattr', ['-d', 'com.apple.quarantine', chromeAppPath]);
   }
 
   return RevisionInfo(
