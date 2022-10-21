8a7a07697c0b5e2adb972d33fbd50267257caffb
Xavier Hainaux <xavier.hainaux@gmail.com>
2022-10-21 11:13:13 +0200

    Fixes

--- test/connect_on_web_part.dart
+++ test/connect_on_web_part.dart
@@ -3,13 +3,12 @@ import 'package:puppeteer/puppeteer.dart';
 
 void main() async {
   var endpoint = document.body!.attributes['x-puppeteer-endpoint'];
-  print("Got endpoint: $endpoint");
   var browser = await puppeteer.connect(browserWsEndpoint: endpoint);
 
   var currentPage = await browser.targets
       .firstWhere((t) => t.url.endsWith('/index.html'))
       .page;
-  print("Got page ${await currentPage.title} ${browser.targets.length}");
+  print('Got page ${await currentPage.title} ${browser.targets.length}');
   await currentPage.evaluate('''
 () => {  
     console.log("Hello from puppeteer in js");
