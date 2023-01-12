8eec95cc2f1f3ac79f9f863ce843e0f9a92c814e
Xavier Hainaux <xavier.hainaux@gmail.com>
2023-01-12 14:32:34 +0100

    Add download_chromium binary

--- test/page_test.dart
+++ test/page_test.dart
@@ -246,8 +246,13 @@ void main() {
       expect(await page.evaluate('() => window.events'),
           equals(['prompt', 'denied', 'granted']));
       await context.clearPermissionOverrides();
-      expect(await page.evaluate('() => window.events'),
-          equals(['prompt', 'denied', 'granted'/*, 'prompt' TODO(xha): re-enable once fixed in Chromium*/]));
+      expect(
+          await page.evaluate('() => window.events'),
+          equals([
+            'prompt',
+            'denied',
+            'granted' /*, 'prompt' TODO(xha): re-enable once fixed in Chromium*/
+          ]));
     });
     test('should isolate permissions between browser contexts', () async {
       await page.goto(server.emptyPage);
