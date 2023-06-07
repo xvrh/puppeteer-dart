e1924e48e6627ba29ce7cf7f29d4e5e072c9a5c1
Xavier H <xavier.hainaux@gmail.com>
2023-06-07 20:18:02 +0200

    Bump chrome version (#240)

--- lib/src/downloader.dart
+++ lib/src/downloader.dart
@@ -4,7 +4,7 @@ import 'package:archive/archive.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 
-const _lastVersion = '114.0.5735.45';
+const _lastVersion = '114.0.5735.90';
 
 class DownloadedBrowserInfo {
   final String executablePath;
