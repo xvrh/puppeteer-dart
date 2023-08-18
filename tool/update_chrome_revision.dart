2e049060feb5250ce7f7e2e73140a8e7f87c3a03
Xavier Hainaux <xavier.hainaux@gmail.com>
2023-08-19 00:02:09 +0200

    Update chrome script

--- tool/update_chrome_revision.dart
+++ tool/update_chrome_revision.dart
@@ -3,49 +3,41 @@ import 'dart:io';
 
 import 'package:github_actions_toolkit/github_actions_toolkit.dart' as gaction;
 import 'package:http/http.dart';
-import 'package:version/version.dart';
 
 final _downloaderFile = File('lib/src/downloader.dart');
 final _variablePrefix = 'const _lastVersion = ';
 final _versionExtractor = RegExp("$_variablePrefix'([^']+)';");
-String _replaceVersion(Version version) => "$_variablePrefix'$version';";
+String _replaceVersion(String version) => "$_variablePrefix'$version';";
 
 void main() async {
   var stableVersion = await _getVersionAndRevisionForStable();
   var currentVersion = await _readCurrentVersion();
 
-  if (stableVersion > currentVersion) {
-    var message = 'roll to Chrome $stableVersion';
-    gaction.setOutput('commit', message);
+  var message = 'roll to Chrome $stableVersion';
+  gaction.setOutput('commit', message);
 
-    await _updateCurrentVersion(stableVersion);
-  } else {
-    print('No update needed. Current: $currentVersion, stable: $stableVersion');
-  }
+  await _updateCurrentVersion(stableVersion);
 }
 
-Future<Version> _getVersionAndRevisionForStable() async {
+Future<String> _getVersionAndRevisionForStable() async {
   var result = await read(Uri.parse(
       'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json'));
   var json = jsonDecode(result);
   //ignore: avoid_dynamic_calls
   var version = json['channels']['Stable']['version'] as String;
 
-  return Version.parse(version);
+  return version;
 }
 
-Future<Version> _readCurrentVersion() async {
+Future<String> _readCurrentVersion() async {
   var content = await _downloaderFile.readAsString();
 
-  var versionString = _versionExtractor.firstMatch(content)!.group(1)!;
-  return Version.parse(versionString);
+  return _versionExtractor.firstMatch(content)!.group(1)!;
 }
 
-
-Future<void> _updateCurrentVersion(Version newVersion) async {
+Future<void> _updateCurrentVersion(String newVersion) async {
   var content = await _downloaderFile.readAsString();
-  var newContent = content.replaceFirstMapped(content, (match) {
-    return _replaceVersion(newVersion);
-  });
+  var newContent =
+      content.replaceFirst(_versionExtractor, _replaceVersion(newVersion));
   await _downloaderFile.writeAsString(newContent);
 }
