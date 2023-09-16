2b81eb831c082bb5a008e918f3c914047dc92b4e
xvrh <xvrh@users.noreply.github.com>
2023-09-16 06:04:55 +0000

    roll to Chrome 117.0.5938.88

--- lib/src/downloader.dart
+++ lib/src/downloader.dart
@@ -4,7 +4,7 @@ import 'package:archive/archive.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 
-const _lastVersion = '117.0.5938.62';
+const _lastVersion = '117.0.5938.88';
 
 class DownloadedBrowserInfo {
   final String executablePath;
