39093c12667f0134d584a0840875ca1a846e3060
Xavier H. <xavier.hainaux@gmail.com>
2024-08-14 09:15:14 +0200

    roll to Chrome 127.0.6533.119 (#335)
    Automatically generated by
    https://github.com/xvrh/puppeteer-dart/blob/main/.github/workflows/update-browser-pins.yaml
    
    Co-authored-by: xvrh <xvrh@users.noreply.github.com>

--- lib/src/downloader.dart
+++ lib/src/downloader.dart
@@ -4,7 +4,7 @@ import 'package:archive/archive.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 
-const _lastVersion = '127.0.6533.99';
+const _lastVersion = '127.0.6533.119';
 
 class DownloadedBrowserInfo {
   final String executablePath;
