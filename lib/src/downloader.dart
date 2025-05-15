bb06481c3149724d080941ef448f4ea9d7ca3777
Xavier H. <xavier.hainaux@gmail.com>
2025-05-15 09:03:52 +0200

    roll to Chrome 136.0.7103.94 (#394)
    Automatically generated by
    https://github.com/xvrh/puppeteer-dart/blob/main/.github/workflows/update-browser-pins.yaml
    
    Co-authored-by: xvrh <xvrh@users.noreply.github.com>

--- lib/src/downloader.dart
+++ lib/src/downloader.dart
@@ -4,7 +4,7 @@ import 'package:archive/archive.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 
-const _lastVersion = '136.0.7103.92';
+const _lastVersion = '136.0.7103.94';
 
 class DownloadedBrowserInfo {
   final String executablePath;
